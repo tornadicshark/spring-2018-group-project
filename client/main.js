0902f27b2118c2a31153ea9e21edf909589ed52b
kwalke79 <kwalke79@uncc.edu>
2018-04-18 23:01:32 -0400

    Basic routing working with the exception of the lexApp module -  To be resolved at a future time

--- client/main.js
+++ client/main.js
@@ -1,13 +1,58 @@
+// GENERIC IMPORTS
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 
-import '../imports/startup/accounts-config.js';
-import dashboardApp from '../imports/components/dashboard';
+import ngRoute from 'angular-route'; // routing import
+import '../imports/startup/accounts-config.js'; // user account import
+
+// SITEWIDE TOOLS
 import sidebarApp from '../imports/components/sidebar';
- 
-angular.module('cryptodash', [
+import faqApp from '../imports/components/faq';
+import stocksApp from '../imports/components/stocks';
+//import lexApp from '../imports/components/lex';
+
+// AUTHENTICATED USER TOOLS
+import dashboardApp from '../imports/components/dashboard';
+
+// CRYPTODASH MODULE SET UP
+var app = angular.module('cryptodash', [
+  // GENERIC IMPORTS
   angularMeteor,
   'accounts.ui',
-  dashboardApp.name,
-  sidebarApp.name
+  ngRoute,
+
+  // SITEWIDE IMPORTS
+  sidebarApp.name,
+  faqApp.name,
+  stocksApp.name,
+  //lexApp.name,
+
+  // AUTHENTICATED USER IMPORTS
+  dashboardApp.name
 ]);
+
+// CRYPTODASH MODULE ROUTING CONFIGURATION
+app.config(function($routeProvider) {
+  $routeProvider
+  .when("/", {
+    template: `<stocks-app></stocks-app>`
+  })
+  .when("/dashboard", {
+    template: `<dash-app></dash-app>`
+  })
+  .when("/stocks", {
+    redirectTo: '/'
+  }) /*
+  .when("/lex", {
+    template: `<lex-app></lex-app>`
+  }) */
+  .when("/faq", {
+    template: `<faq-app></faq-app>`
+  })
+  .when("/error", {
+    template: `404. That page is not found.`
+  })
+  .otherwise({
+    redirectTo: '/error'
+  });
+});
