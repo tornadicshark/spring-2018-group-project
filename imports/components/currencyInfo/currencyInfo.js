a12169cb08b5addf18b0dd4a69b3d4ca50226808
kwalke79 <kwalke79@uncc.edu>
2018-04-25 02:14:08 -0400

    WIP update for homepage

--- imports/components/currencyInfo/currencyInfo.js
+++ imports/components/currencyInfo/currencyInfo.js
@@ -15,7 +15,7 @@ class CurrencyCtrl {
           }
         })
     }
-
+/*
   buyStock(name, value, owner) {
     console.log("buy stock button clicked");
     console.log(name + ", " + value + ", " + owner);
@@ -24,14 +24,61 @@ class CurrencyCtrl {
   sellStock(name, value, owner) {
     console.log("sell stock button clicked");
     console.log(name + ", " + value + ", " + owner);
-  }
+  }*/
   
 }
  
 export default angular.module('CurrencyApp', [
   angularMeteor
 ])
-  .component('currencyInfo', {
+.component('currencyInfo', {
     templateUrl: 'imports/components/currencyInfo/currencyInfo.html',
     controller: ['$scope', CurrencyCtrl]
-  });
+}).controller('numberSpinner', ['$scope', function($scope){
+    var vm = this;
+    
+    vm.testNumber = 10;  
+}])
+.directive('numericOnly', function(){
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, modelCtrl) {
+
+            modelCtrl.$parsers.push(function (inputValue) {
+                var transformedInput = inputValue ? inputValue.replace(/[^\d.-]/g,'') : null;
+
+                if (transformedInput!=inputValue) {
+                    modelCtrl.$setViewValue(transformedInput);
+                    modelCtrl.$render();
+                }
+
+                return transformedInput;
+            });
+        }
+    };
+}).directive('numberSpin', [function(){  
+    return {
+        restrict: 'E',
+        scope: {
+            "ngModel": '='
+        },
+        template: '<div>' +
+        '<input numeric-only data-ng-model="ngModel" ng-pattern="onlyNumbers" type="text">'+
+        '<a class="ns-plus"  data-ng-click="plus()">+</a>' +
+        '<a class="ns-minus"data-ng-click="minus()">-</a> </div>',
+        link: function(scope, elem, attrs){
+        
+            scope.onlyNumbers = /^\d+$/;
+        
+            scope.plus = function(){
+                scope.ngModel = scope.ngModel + 1;
+            }
+            
+            scope.minus = function(){
+                scope.ngModel = scope.ngModel - 1;
+            }
+        
+        
+        }
+    }
+}]);
